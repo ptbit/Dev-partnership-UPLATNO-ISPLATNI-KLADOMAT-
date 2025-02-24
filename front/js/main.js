8a594397f4d0e3174182dd9435a0bcfe15a84fbc
foxstudent103376 <yashopua@gmail.com>
2025-02-24 16:20:35 +0200

    js

--- front/js/main.js
+++ front/js/main.js
@@ -1,5 +1,4 @@
 document.querySelector('.hero__btn').addEventListener('click', function () {
-  console.log('click1');
   const targetElement = document.getElementById('result');
 
   const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - 70;
@@ -9,8 +8,8 @@ document.querySelector('.hero__btn').addEventListener('click', function () {
     behavior: 'smooth',
   });
 });
+
 document.querySelector('.service__btn').addEventListener('click', function () {
-  console.log('click2');
   const targetElement = document.getElementById('result');
 
   const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - 70;
@@ -21,17 +20,3 @@ document.querySelector('.service__btn').addEventListener('click', function () {
   });
 });
 
-//THEME CHANGER
-const body = document.querySelector('body');
-const themeBtn = document.getElementById('theme');
-
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme) {
-  body.classList.add(savedTheme);
-}
-
-themeBtn.addEventListener('click', () => {
-  body.classList.toggle('dark');
-  const currentTheme = body.classList.contains('dark') ? 'dark' : '';
-  localStorage.setItem('theme', currentTheme);
-});
